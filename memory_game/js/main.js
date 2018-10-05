f480c6447ea256332b8e2b87e0311091dd266708
Brian Ogilvie <brionline781@mac.com>
2018-10-05 10:26:05 -0400

    added JS to render game actions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,17 +31,30 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId].rank);
+	var srcPath = cards[cardId].cardImage;
+	this.setAttribute('src', srcPath);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 };
 
-flipCard(0);
-flipCard(3);
+var createBoard = function() {
+	var gameBoard = document.getElementById('game-board');
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		gameBoard.appendChild(cardElement);
+	}
+};
+
+createBoard();
